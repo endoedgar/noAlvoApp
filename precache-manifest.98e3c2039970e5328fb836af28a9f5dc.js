25ba41e0d8ecfb96695e6c135ce2f2896b452a1e
Edgar de Jesus Endo Junior <endoedgarjunior@gmail.com>
2020-03-11 11:20:46 -0500

    Updates

---
+++ precache-manifest.98e3c2039970e5328fb836af28a9f5dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f2ee526cb27585b5b074b018c4ed21",
+    "url": "/noAlvoApp/index.html"
+  },
+  {
+    "revision": "0888a159995a6cdf0b46",
+    "url": "/noAlvoApp/static/css/2.dcab9049.chunk.css"
+  },
+  {
+    "revision": "1a51f37c4a0b6f57bff7",
+    "url": "/noAlvoApp/static/css/main.1563db02.chunk.css"
+  },
+  {
+    "revision": "0888a159995a6cdf0b46",
+    "url": "/noAlvoApp/static/js/2.57edd233.chunk.js"
+  },
+  {
+    "revision": "6a3ae45f7cd1c3f49e457639652350f2",
+    "url": "/noAlvoApp/static/js/2.57edd233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a51f37c4a0b6f57bff7",
+    "url": "/noAlvoApp/static/js/main.09118110.chunk.js"
+  },
+  {
+    "revision": "44a084e6c52c7d6ee4e4",
+    "url": "/noAlvoApp/static/js/runtime-main.7436005a.js"
+  }
+]);
